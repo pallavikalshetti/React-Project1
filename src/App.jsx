3dbef46f1ef4c1c1fe625e6143cb205f0b96e66b
pallavikalshetti <kannur.pallavi@outlook.com>
2025-08-13 21:32:34 -0400

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import UrlShortener from './components/UrlShortener';
 import HeaderPage from './components/Header';
 import Hero from './components/Hero';
-import FooterSection from './components/footerSection';
+import FooterSection from './components/FooterSection';
 import StatsSection from './components/StatsSection';
 import Footer from './components/Footer';
 import './App.css';
